145cf266c116aaabf30e957037dac32ae384b405
vahid-bagh3ri <vahid.bagheri1379@gmail.com>
2020-10-03 22:49:44 +0330

    let in EC6

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function func(z,y) {
+    let x =z;
+        if (true) {
+            let x = 'vahid'
+            console.log(x)
+            console.log(y)
+        }
+
+        console.log(x)
+}
+
+func()
+
+
+var array = [1,2,3,4];
+for (let index = 0; index < array.length; index++) {
+    console.log(index);
+}
+
+
+// -let 
