4d7ba2e4257f0f2a2295f48e29d7da5c30ff718a
vahid-bagh3ri <vahid.bagheri1379@gmail.com>
2020-10-14 18:04:27 +0330

    static methods

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ class Car{
     this.year = year 
     }
 
-    run(){
+    static run(){
         return ' speed up to 200'
     }
 }
@@ -28,14 +28,37 @@ class IranCar extends Car {
         super(make , model ,year)
         this.color = color
     }
-
+    
     fly(){
         return 'car is flying'
     }
-    go(){
-        return this.run
+
+    static run(){
+        return ' speed up to 40'
+    }
+
+    static go(){
+        // console.log(this);
+        return this.run()
     }
 }
 
 let car1 = new IranCar ("ford", "mustung" , 2000 , 'red' )
 console.log(car1);
+// console.log(car1.go());
+
+
+// console.log(IranCar.go());
+
+
+class AdvanceArray extends Array{
+    find(value){
+        let val = this.filter(item => item === value);
+        return val.length === 0 ? 'error' : val[0];
+    }
+}
+
+let list = new AdvanceArray('vahid' , 'pes' , 'mmd')
+
+console.log(list.find("vahid"));
+console.log(list.reverse());
