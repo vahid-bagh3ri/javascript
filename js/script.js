cc2cff43561d28ac87f86e8b1d5a30bffa25bf20
vahid-bagh3ri <vahid.bagheri1379@gmail.com>
2020-10-15 18:58:59 +0330

    symbols

--- js/script.js
+++ js/script.js
@@ -1,14 +1,55 @@
 
-let name = ' vahid'
-let keyname = 'fullname'
 
-let obj ={
-    name :name,
-    [keyname]: "vahid bagheri",
-    'say hello'() {
-        return 'hello vahid'
+
+// let symbol1 = Symbol('fullname')
+// let symbol2 = Symbol('fullname')
+
+
+
+// let obj = {
+//     name: 'vahid' ,
+//     [symbol1] : 22
+// }
+
+
+// console.log(obj);
+
+// for (let o in obj) {
+//     console.log(o);
+    
+// }
+
+// console.log(obj[Symbol('fullname')]);
+// console.log(symbol1 == symbol2);
+
+// ------------------------------------------------------------
+
+// let symbol1 = Symbol.for('fullname')
+
+// let obj = {
+//     name : 'vahid' , 
+//     [symbol1] : 22
+// }
+
+// let symbolN = Symbol.for('fullname')
+
+
+// console.log(symbol1 == symbolN);
+
+// console.log(obj[symbolN]);
+
+class ReplaceX {
+    constructor(value){
+        this.value = value
+    }
+
+    // [Symbol.replace](string){
+    //     return 'vahid'
+    // }
+
+    [Symbol.replace](string){
+        return `/${string}/${this.value}`
     }
 }
 
-
-console.log(obj);
+console.log('foo'.replace(new ReplaceX('bar')));
