3fb0b88564c4e80c4bd3ca44adaf007a8e80ec42
vahid-bagh3ri <vahid.bagheri1379@gmail.com>
2020-10-15 09:05:21 +0330

    getter and setter

--- js/script.js
+++ js/script.js
@@ -2,4 +2,26 @@ class Foo {
     constructor(){
         this.log = []
     }
+
+    set current(value){
+        this.log.push(value)
+    }
+
+
+    get latest(){
+        if (this.log.length === 0) {
+            return undefined
+        }
+        return this.log[this.log.length - 1]
+    }
 }
+
+
+
+
+let bar = new Foo();
+bar.current='A';
+bar.current= 'b';
+
+
+console.log(bar.latest);
